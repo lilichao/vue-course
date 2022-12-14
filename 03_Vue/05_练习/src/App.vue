f89241c625edf23ce88e04fa783bbb1544a8b790
lilichao <27254174@qq.com>
2022-12-14 19:44:42 +0800

    完成基本布局

--- 03_Vue/05_练习/src/App.vue
+++ 03_Vue/05_练习/src/App.vue
@@ -27,7 +27,54 @@ const current = ref(0) // 0 表示球员， 1 表示球队
             </div>
         </header>
         <!-- 选项卡的主体 -->
-        <div class="main"></div>
+        <div class="main">
+            <!-- 
+                current === 0 显示球员
+                current === 1 显示球队
+                    v-show 指令，用来设置一个内容是否显示
+                        v-show是通过display来设置一个元素是否显示的
+            -->
+            <div v-show="current === 0">
+                <!-- 球员 -->
+                <div class="tab-list">
+                    <div class="tab-item">
+                        <!-- 图片 -->
+                        <div class="photo">
+                            <img src="/images/messi.png" alt="梅西" />
+                            <span>1</span>
+                        </div>
+
+                        <!-- 描述 -->
+                        <div class="desc">
+                            <span class="name">梅西</span>
+                            <div class="hot-bar">
+                                <div class="inner">433760热度</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div v-show="current === 1">
+                <!-- 球队 -->
+                <div class="tab-list">
+                    <div class="tab-item">
+                        <!-- 图片 -->
+                        <div class="photo">
+                            <img src="/images/法国.jpg" alt="法国" />
+                            <span>1</span>
+                        </div>
+
+                        <!-- 描述 -->
+                        <div class="desc">
+                            <span class="name">法国</span>
+                            <div class="hot-bar">
+                                <div class="inner">333760热度</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -62,5 +109,75 @@ const current = ref(0) // 0 表示球员， 1 表示球队
 .active {
     background-color: rgb(187, 3, 52);
     color: #fff;
+}
+
+.tab-list {
+    margin: 20px;
+}
+
+.tab-item {
+    display: flex;
+    margin-bottom: 40px;
+}
+
+/* 容器 */
+.photo {
+    width: 150px;
+    background-color: #fff;
+    border-radius: 20px;
+    overflow: hidden;
+    position: relative;
+}
+
+/* 图片 */
+.photo img {
+    width: 100%;
+    vertical-align: top;
+}
+
+/* 排名 */
+.photo span {
+    position: absolute;
+    width: 50px;
+    height: 50px;
+    background-color: rgb(245, 102, 1);
+    top: 0;
+    left: 0;
+    font-size: 20px;
+    font-weight: bold;
+    color: #fff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-bottom-right-radius: 20px;
+    border-top-left-radius: 20px;
+}
+
+.desc {
+    display: flex;
+    flex-flow: column;
+    justify-content: space-evenly;
+    font-size: 30px;
+    color: #fff;
+    flex: auto;
+    margin-left: 30px;
+}
+
+.hot-bar {
+    background-color: rgb(3, 37, 103);
+    border-radius: 20px;
+    text-indent: 0.5em;
+    overflow: hidden;
+}
+
+.inner {
+    border-radius: 20px;
+    background-image: linear-gradient(
+        90deg,
+        rgb(187, 3, 52) 50%,
+        rgb(66, 2, 12)
+    );
+    width: 10%;
+    white-space: nowrap;
 }
 </style>
