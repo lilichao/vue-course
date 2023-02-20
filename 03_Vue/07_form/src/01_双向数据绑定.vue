adfdd9c2b3b581d7c9f62da7c15ca025f094858a
lilichao <lilichao1988@gmail.com>
2023-02-21 00:33:30 +0800

    v-model

---
+++ 03_Vue/07_form/src/01_双向数据绑定.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from "vue"
+let text = ref("")
+
+function submitHandler() {
+    console.log(text.value)
+    // 将text提交给服务器，在根据服务器返回的数据做后续的操作
+}
+</script>
+
+<template>
+    <h1>Hello Vue</h1>
+    <form @submit.prevent="submitHandler">
+        <!-- 
+        这里我们将表单项的value属性和变量text做了绑定，
+          当value发生变化时，text变量会随之变化（单向绑定）
+
+          当value或text任意一个发生变化，另一个也会随之变化（双向绑定）
+      -->
+        <div>
+            <input
+                type="text"
+                @input="(event) => (text = event.target.value)"
+                :value="text"
+            />
+        </div>
+        <div>
+            <input
+                type="text"
+                @input="(event) => (text = event.target.value)"
+                :value="text"
+            />
+        </div>
+        <div>
+            <button>提交</button>
+        </div>
+    </form>
+</template>
+
+<style scoped></style>
--- 03_Vue/07_form/src/App.vue
+++ 03_Vue/07_form/src/App.vue
@@ -1,6 +1,10 @@
 <script setup>
 import { ref } from "vue"
-let text = ref("")
+const text = ref("")
+const bool = ref("是")
+const hobbies = ref([])
+const gender = ref("女")
+const friend = ref("猪八戒")
 
 function submitHandler() {
     console.log(text.value)
@@ -12,24 +16,60 @@ function submitHandler() {
     <h1>Hello Vue</h1>
     <form @submit.prevent="submitHandler">
         <!-- 
-        这里我们将表单项的value属性和变量text做了绑定，
-          当value发生变化时，text变量会随之变化（单向绑定）
+        在vue中，为我们提供了v-model了可以快速完成表单的双向数据绑定
 
-          当value或text任意一个发生变化，另一个也会随之变化（双向绑定）
+        <input type="text" v-model="text" />
+        <input type="text" :value="text" @input="event => text = event.target.value" />
       -->
+        <div>信息：<input type="text" v-model="text" /></div>
         <div>
-            <input
-                type="text"
-                @input="(event) => (text = event.target.value)"
-                :value="text"
+            是否：<input
+                type="checkbox"
+                v-model="bool"
+                true-value="是"
+                false-value="否"
             />
         </div>
         <div>
+            爱好：
             <input
-                type="text"
-                @input="(event) => (text = event.target.value)"
-                :value="text"
-            />
+                v-model="hobbies"
+                type="checkbox"
+                name="hobby"
+                value="足球"
+            />足球
+            <input
+                v-model="hobbies"
+                type="checkbox"
+                name="hobby"
+                value="篮球"
+            />篮球
+            <input
+                v-model="hobbies"
+                type="checkbox"
+                name="hobby"
+                value="羽毛球"
+            />羽毛球
+            <input
+                v-model="hobbies"
+                type="checkbox"
+                name="hobby"
+                value="乒乓球"
+            />乒乓球
+        </div>
+        <div>
+            性别：
+            <input v-model="gender" type="radio" name="gender" value="男" />男
+            <input v-model="gender" type="radio" name="gender" value="女" />女
+        </div>
+        <div>
+            朋友：
+            <select v-model="friend">
+                <option disabled value="">请选择你的好朋友...</option>
+                <option>孙悟空</option>
+                <option>猪八戒</option>
+                <option>沙和尚</option>
+            </select>
         </div>
         <div>
             <button>提交</button>
