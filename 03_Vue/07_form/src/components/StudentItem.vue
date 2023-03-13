b1c501e9841da24118059253060cf04c3cdb75b9
lilichao <lilichao1988@gmail.com>
2023-03-13 22:26:37 +0800

    引出透传的问题

---
+++ 03_Vue/07_form/src/components/StudentItem.vue
@@ -0,0 +1,38 @@
+<script setup>
+const props = defineProps(["students"])
+const emits = defineEmits(["delStu"])
+
+// 定义一个删除学生的方法
+const delStuHandler = (index) => {
+    if (confirm("该操作不可恢复，请确认！")) {
+        // 删除当前的学生
+        // props.students.splice(index, 1)
+        // props.delStu(index)
+        emits("delStu", index)
+    }
+}
+</script>
+
+<template>
+    <tbody>
+        <tr v-for="(stu, index) in props.students">
+            <td>{{ stu.id }}</td>
+            <td>{{ stu.name }}</td>
+            <td>{{ stu.age }}</td>
+            <td>{{ stu.gender }}</td>
+            <td>{{ stu.address }}</td>
+            <td>
+                <!-- 在模板中可以通过$emit()来触发自定义事件 -->
+                <!-- <a href="#" @click.prevent="$emit('delStu', index)">删除</a> -->
+                <a href="#" @click.prevent="delStuHandler(index)">删除</a>
+            </td>
+        </tr>
+    </tbody>
+</template>
+
+<style scoped>
+td {
+    border: 1px solid black;
+    font-size: 24px;
+}
+</style>
--- 03_Vue/07_form/src/components/StudentList.vue
+++ 03_Vue/07_form/src/components/StudentList.vue
@@ -1,18 +1,11 @@
 <script setup>
 // 通过defineProps定义的属性在attrs中就不存在了
-// 使用自定义属性时，最好通过defineProps来声明一下
-const props = defineProps(["students"])
-const emits = defineEmits(["delStu"])
 
-// 定义一个删除学生的方法
-const delStuHandler = (index) => {
-    if (confirm("该操作不可恢复，请确认！")) {
-        // 删除当前的学生
-        // props.students.splice(index, 1)
-        // props.delStu(index)
-        emits("delStu", index)
-    }
-}
+import StudentItem from "./StudentItem.vue"
+
+// 使用自定义属性时，最好通过defineProps来声明一下
+// const props = defineProps(["students"])
+// const emits = defineEmits(["delStu"])
 </script>
 <template>
     <table>
@@ -29,20 +22,11 @@ const delStuHandler = (index) => {
                 <th>操作</th>
             </tr>
         </thead>
-        <tbody>
-            <tr v-for="(stu, index) in props.students">
-                <td>{{ stu.id }}</td>
-                <td>{{ stu.name }}</td>
-                <td>{{ stu.age }}</td>
-                <td>{{ stu.gender }}</td>
-                <td>{{ stu.address }}</td>
-                <td>
-                    <!-- 在模板中可以通过$emit()来触发自定义事件 -->
-                    <!-- <a href="#" @click.prevent="$emit('delStu', index)">删除</a> -->
-                    <a href="#" @click.prevent="delStuHandler(index)">删除</a>
-                </td>
-            </tr>
-        </tbody>
+
+        <StudentItem
+            :students="$attrs.students"
+            @del-stu="$attrs.onDelStu"
+        ></StudentItem>
     </table>
 </template>
 
@@ -58,8 +42,7 @@ caption {
     font-weight: bold;
 }
 
-th,
-td {
+th {
     border: 1px solid black;
     font-size: 24px;
 }
