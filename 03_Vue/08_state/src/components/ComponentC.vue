e65445b3c225ad9903b092682ddf6844f06298ee
lilichao <lilichao1988@gmail.com>
2023-03-16 09:39:30 +0800

    state的修改

--- 03_Vue/08_state/src/components/ComponentC.vue
+++ 03_Vue/08_state/src/components/ComponentC.vue
@@ -15,19 +15,50 @@ const stuStore = useStudentStore()
 
     可以通过storeToRefs()来对store进行解构，
         它可以将state和getters中的属性解构为ref属性，从而保留其响应性
+
+    state的修改：
+        1. 直接修改
+        2. 通过$patch
+        3. 通过$patch传函数的形式的修改
+        4. 直接替换state
+        5. 重置state
 */
 // const { name, age } = stuStore
 
 const { name, age, title } = storeToRefs(stuStore)
 
 // state中的属性（方法），都可以通过store对象直接访问
+
+const clickHandler = () => {
+    // stuStore.$patch({
+    //     name: "孙小圣",
+    //     age: 20,
+    //     skills: ["救命毫毛"]
+    // })
+
+    // stuStore.$patch((state) => {
+    //     state.skills.push("救命毫毛")
+    // })
+
+    // stuStore.skills.push("哈哈")
+
+    // stuStore.$patch({ name: "孙小圣" })
+    stuStore.$state = { name: "孙小圣" }
+}
 </script>
 <template>
     <h4>
-        ComponentC -- {{ name }} -- {{ age }} -- {{ title }}
+        ComponentC -- {{ name }} -- {{ age }} -- {{ title }} --
+        {{ stuStore.skills }}
         <!-- {{ countStore.count }} --
         <button @click="countStore.increment">按钮</button> -->
 
         <button @click="stuStore.growUp">长大</button>
+
+        <hr />
+
+        <button @click="stuStore.name = '孙大圣'">修改name</button>
+        <button @click="clickHandler">patch修改</button>
+        <button @click="stuStore.$reset()">重置</button>
     </h4>
 </template>
