efbfac54d57261f8004aaf22fbaab3a1bf0ce142
lilichao <lilichao1988@gmail.com>
2023-04-05 15:46:12 +0800

    结账页面的bar

--- 03_Vue/09_meals/src/components/Checkout/Checkout.vue
+++ 03_Vue/09_meals/src/components/Checkout/Checkout.vue
@@ -36,6 +36,23 @@ const meals = useMealsStore()
                 合计￥<span>{{ meals.amount }}</span>
             </footer>
         </div>
+
+        <div class="bar">
+            <div class="total-amount">
+                <p v-show="meals.totalCount <= 0" class="no-goods">
+                    未选购商品
+                </p>
+                <p
+                    @click="showDetails = true"
+                    v-show="meals.totalCount > 0"
+                    class="has-goods"
+                >
+                    {{ meals.amount }}
+                </p>
+            </div>
+
+            <button class="checkout-btn">去支付</button>
+        </div>
     </div>
 </template>
 
@@ -139,5 +156,50 @@ const meals = useMealsStore()
 .amount::before {
     content: "￥";
     font-weight: normal;
+}
+
+.bar {
+    width: 710rem;
+    height: 100rem;
+    position: fixed;
+    background-color: rgb(58, 58, 58);
+    bottom: 40rem;
+    left: 0;
+    right: 0;
+    margin: 0 auto;
+    border-radius: 60rem;
+}
+
+.total-amount {
+    margin-left: 40rem;
+    line-height: 100rem;
+}
+
+.no-goods,
+.has-goods {
+    color: rgb(148, 148, 148);
+    font-size: 36rem;
+    font-weight: bold;
+}
+
+.has-goods {
+    color: #fff;
+}
+
+.has-goods::before {
+    content: "合计：￥";
+    font-size: 26rem;
+}
+
+.checkout-btn {
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 200rem;
+    height: 100%;
+    border-radius: 60rem;
+    border: none;
+    background-color: rgb(248, 188, 0);
+    font-size: 36rem;
 }
 </style>
